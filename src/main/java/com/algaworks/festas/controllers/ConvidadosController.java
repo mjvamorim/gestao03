856fab2e1d15d2b7b7b1d7f438bfc66b1a869d7a
Mauricio Amorim <mjvamorim@gmail.com>
2019-05-20 21:38:11 -0300

    Layout

--- src/main/java/com/algaworks/festas/controllers/ConvidadosController.java
+++ src/main/java/com/algaworks/festas/controllers/ConvidadosController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,6 +32,21 @@ public class ConvidadosController {
 	public String salvar(Convidado c) {
 		cr.save(c);
 		return "redirect:/convidados";
+	}
+	
+	@RequestMapping("/excluir/{id}")
+	public String excluir(@PathVariable Long id) {
+		cr.deleteById(id);
+		return "redirect:/convidados";
+	}
+	
+	@RequestMapping("/alterar/{id}")
+	public ModelAndView alterar(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView("ListaConvidados");
+		mv.addObject("convidados",cr.findAll());
+		Convidado c = cr.getOne(id);
+		mv.addObject( "convidado",c);
+		return (mv);
 	}
 	
 	
